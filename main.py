5b73768fd01de2f6f6ec8b5012c8587729898f6d
Анатолий <lan2002@yandex.ru>
2024-07-19 05:20:32 +0300

    (new) main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import requests
+
+THE_CAT_API_KEY = "live_fTy0RIfPJYRR3CTUfo2MriNMXGckcGyeXFTVGuRXHIUGrCgWpkB3c5EnzdHAViiL"
+
+
+# Функция, которая делает запрос к TheCatAPI для получения случайного изображения кошки
+def get_cat_image_url():
+    url = f"https://api.thecatapi.com/v1/images/search"
+    headers = {"x-api-key": THE_CAT_API_KEY}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
